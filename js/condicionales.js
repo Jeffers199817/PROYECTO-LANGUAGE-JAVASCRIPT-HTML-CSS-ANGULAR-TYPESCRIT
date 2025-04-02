7d796997d9176724f82f638bf3286f03c3217825
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-04-02 16:26:10 -0500

    Integración de las configuraciónes de condicionales if-else

---
+++ js/condicionales.js
@@ -0,0 +1,9 @@
+var aprobe = true;
+
+if (aprobe == true) { 
+    console.log("Puedo Sallir");
+    
+} else { 
+    console.log("Tengo que estudiar más has reprobado.");
+}
+
---
+++ js/ejemplo.js
@@ -0,0 +1,21 @@
+
+const restaurante = "Bienvenido al Catador";
+var comida1 = 2800;
+var comida2 = 1300;
+
+var bebida1 = 900;
+var bebida2 = 650;
+
+console.log("La comida 1 su precio es: " + comida1);
+
+console.log("La comida 2 su precio es: " + comida2);
+
+console.log("Total de comida: " + (comida1+comida2))
+
+console.log("La bebida 1 su precio es: " + bebida1);
+
+console.log("La bebida 2  su precio es: " + bebida2);
+
+console.log("Total de bebida: " + (bebida1 + bebida2));
+
+console.log("Su cuenta total es: " + (comida1 + comida2 + bebida1 + bebida2));
