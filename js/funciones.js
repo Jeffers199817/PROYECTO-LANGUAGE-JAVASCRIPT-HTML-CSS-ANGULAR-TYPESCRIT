f6d2475cfd4068ab11b8d05c5d93fe83dcd4b1a6
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-04-03 11:08:06 -0500

    Integración de funciones

---
+++ js/funciones.js
@@ -0,0 +1,13 @@
+
+let nombre =prompt("¿Ya te suscribiste a milenyumsoft?");
+
+alert(nombre);
+
+
+let suscribirse = confirm("¿Quiere suscribirse a mylueumsoft?")
+
+if (suscribirse == true) {
+    alert("Eres un pro gracis.")
+} else { 
+    alert("ya nada.")
+}
