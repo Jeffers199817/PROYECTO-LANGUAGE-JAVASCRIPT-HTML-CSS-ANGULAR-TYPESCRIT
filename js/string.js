d736456706b9b4a659b3db7949b94776b698a478
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-04-06 13:54:24 -0500

    Integración de funcitons

---
+++ js/string.js
@@ -0,0 +1,12 @@
+let frase = "Suscribete a TodoCode";
+let fragmento = frase.slice(0, 9);
+
+console.log(fragmento);
+
+let frase1 = "naranja, pechuga, lechuga, tomate";
+
+console.log(frase1);
+
+let lista = frase1.split(",");
+
+console.log(lista);
