5b82bc2245c31c973f49d55ead965476dcb795d8
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-04-05 20:42:43 -0500

    INtegración

---
+++ js/function.js
@@ -0,0 +1,19 @@
+/*function saludar() {
+console.log("Estas lllamdno una función");
+}
+saludar();
+
+*/
+
+function sumar(num1, num2) {
+    
+    let resultado = num1 + num2;
+
+    return resultado;
+}
+
+let suma = sumar(2, 5);
+let suma2 = sumar(23, 5);
+
+console.log("La suma es" + suma);
+console.log("La suma es" + suma2);
