9379a1cd4c3f083bab28009749b241a054dd6e42
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-04-07 13:20:15 -0500

    Integración de objetosString

---
+++ js/objetosString.js
@@ -0,0 +1,23 @@
+
+let frase = "Suscribete a Mylenyum=soft ";
+let longitud = frase.length;
+
+console.log("La longitud es: " + longitud);
+
+
+let recorte = frase.slice(0, 27);
+
+console.log("El recorte es: " + recorte);
+
+
+let frases = "naranja, manzana, lechuga, tomate";
+
+let lista = frases.split(",");
+
+console.log(lista);
+
+
+let mayusculas = frase.toLocaleUpperCase();
+console.log(mayusculas);
+let minusculas = frase.toLocaleLowerCase();
+console.log(minusculas);
